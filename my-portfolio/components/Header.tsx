876b5b633e4ce1f9d6b7a341c641bf2a220d5f7e
NebuFlux <joshua.shoemaker@protonmail.me>
2026-03-08 14:28:20 -0400

    Polished header responsive design

--- my-portfolio/components/Header.tsx
+++ my-portfolio/components/Header.tsx
@@ -12,15 +12,15 @@ export const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div className="relative flex flex-row px-3 h-25 items-end">
+        <div className="relative flex flex-row px-3 h-15 items-end">
             <Image 
             src="https://github.com/NebuFlux.png"
             width={50}
             height={50}
-            className="object-left hidden sm:block rounded-full object-cover border-2 border-secondary"
+            className="object-left rounded-full object-cover border-2 border-secondary"
             alt="GitHub Profile Pic"
             />
-            <h1 className="text-left text-2xl font-bold ml-5 text-primary">Joshua Shoemaker</h1>
+            <h1 className="text-left text-xl md:text-2xl font-bold ml-5 text-primary">Joshua Shoemaker</h1>
             <div className="hidden md:flex flex-row items-end ml-auto gap-10 text-secondary">
                 <Link className={`text-xl transition-transform hover:-translate-y-1 
                 ${pathname ==="/" ? "text-primary border-b-2 border-primary" : ""}`} href="/">
@@ -35,7 +35,7 @@ export const Header = () => {
                     Artifact 2
                 </Link>
                 <Link className={`text-xl transition-transform hover:-translate-y-1 
-                ${pathname ==="/artifact_3" ? "text-primary border-b-2 border-primary" : ""} mr-15`} href="/artifact_3">
+                ${pathname ==="/artifact_3" ? "text-primary border-b-2 border-primary" : ""} mr-10`} href="/artifact_3">
                     Artifact 3
                 </Link>
             </div>
